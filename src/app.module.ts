96799fbdcdf924a2d5c27c8cea5d4d1774041385
denis_levchenko <tranzi90@gmail.com>
2023-11-23 02:52:41 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import * as LocalSession from 'telegraf-session-local';
+
+const sessions = new LocalSession({ database: 'session_db.json' });
+
+@Module({
+  imports: [
+    TelegrafModule.forRoot({
+      middlewares: [sessions.middleware()],
+      token: '',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
